0724bfa8c941977bfdbc771829957f06def3249d
Kamil Lukasiuk <kamilplgt@gmail.com>
2023-03-05 14:47:00 +0100

    added tab icon and userlist/useritem components

---
+++ src/user/components/UsersList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import UserItem from './UserItem';
+import './UsersList.css';
+
+const UserList = (props) => {
+  if (props.items.length === 0) {
+    // if theres no users in database
+    return (
+      <div className='center'>
+        <h2>No users found. Yet!</h2>
+      </div>
+    );
+  }
+  // else return list of users
+  return (
+    <ul className='users-list'>
+      {props.items.map((user) => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default UserList;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-  return <h2>Users Works!!!</h2>;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Kamil',
+      image:
+        'https://samochody-specjalne.pl/wp-content/uploads/2021/02/xp-tgx-eot-individual-lion-s-2_a-678x381.jpg.pagespeed.ic.jQ4OtdHFpg.jpg',
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
